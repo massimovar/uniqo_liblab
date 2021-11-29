8e7570e05859b89dba10fa33bceef0171bb019f7
MassimoVa <m.variolo@asem.it>
2021-11-29 10:42:46 +0100

    Upgraded with 1.0.42.71

--- ProjectFiles/NetSolution/Private/TypeConstants.cs
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -9,18 +9,30 @@ namespace LibLab
 {
     public static class ObjectTypes
     {
-        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("LibLab");
-        public static readonly NodeId animation_tank_level = new NodeId(namespaceIndex, new Guid("ff35424dbd0a462a6f76aebe7210fb1e"));
-        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("fc6c34e088e1f2f4e4347d978a2279da"));
-        public static readonly NodeId page2 = new NodeId(namespaceIndex, new Guid("4d28c948255fcf08a70a69baa6ba311b"));
-        public static readonly NodeId advancedSVGImageType = new NodeId(namespaceIndex, new Guid("43bd9fb016d2f4c22dcac897fff4052a"));
-        public static readonly NodeId RecipesEditorType = new NodeId(namespaceIndex, new Guid("e73026340a8176a848f3b69a172c5be0"));
-        public static readonly NodeId page1 = new NodeId(namespaceIndex, new Guid("c852a7a73047569e593f76527f15438f"));
-        public static readonly NodeId page3 = new NodeId(namespaceIndex, new Guid("798abe481fbd03a6ae59b6cb155b9a22"));
+        public static readonly NodeId animation_tank_level = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("ff35424dbd0a462a6f76aebe7210fb1e"));
+        public static readonly NodeId MainWindow = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("fc6c34e088e1f2f4e4347d978a2279da"));
+        public static readonly NodeId page2 = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("4d28c948255fcf08a70a69baa6ba311b"));
+        public static readonly NodeId advancedSVGImageType = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("43bd9fb016d2f4c22dcac897fff4052a"));
+        public static readonly NodeId RecipesEditorType = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("e73026340a8176a848f3b69a172c5be0"));
+        public static readonly NodeId page1 = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("c852a7a73047569e593f76527f15438f"));
+        public static readonly NodeId page3 = new NodeId(
+            NamespaceMapProvider.GetNamespaceIndex("LibLab"),
+            new Guid("798abe481fbd03a6ae59b6cb155b9a22"));
     }
 
     public static class VariableTypes
     {
-        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("LibLab");
     }
 }
